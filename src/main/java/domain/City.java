4c1e13b974a5aaa153b730cad1a9f7bfa87d913a
Bjorn <bjorn.alfsson@gmail.com>
2018-05-09 23:21:57 +0200

    Pojo class finished!

--- src/main/java/domain/City.java
+++ src/main/java/domain/City.java
@@ -1,5 +1,37 @@
 package java.domain;
 
-public class City {
+import java.io.Serializable;
+import javax.persistence.*;
 
+@Entity
+@Table(name="cities")
+public class City implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  @Column(name="id")
+  private int id;
+  @Column(name="name")
+  private String name;
+  @Column(name="population")
+  private int population;
+
+  public City() {}
+
+  public City(String name, int population) {
+    this.name = name;
+    this.population = population;
+  }
+
+  public String getCityName() {
+    return name;
+  }
+
+  public int getPopulation() {
+    return population;
+  }
+
+  public String toString() {
+    return "The city " + name + " has a population of: " + population;
+  }
 }
